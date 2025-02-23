ccc8a2ee38926cf3ada54e4b5623a51e5f71ccbf
Sebastian L <99sebastianl@gmail.com>
2025-02-23 16:50:52 -0500

    Initial commit - Authentication Success Page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React from "react";
+import "./index.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="card">
+        <img src="/icon.png" alt="VoiceVault Logo" className="logo" />
+        <h1>Authentication Successful</h1>
+        <p>You can now return to the app.</p>
+      </div>
     </div>
   );
 }
